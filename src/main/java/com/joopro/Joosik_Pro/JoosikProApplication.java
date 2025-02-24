d9ddefe80a506fd1b5b0417abb6843aa8b708eb0
junhyukkkk <cjunk0304@gmail.com>
2025-02-24 22:49:33 +0900

    스프링 프로젝트 초기 커밋

---
+++ src/main/java/com/joopro/Joosik_Pro/JoosikProApplication.java
@@ -0,0 +1,13 @@
+package com.joopro.Joosik_Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoosikProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoosikProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joopro/Joosik_Pro/JoosikProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joopro.Joosik_Pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoosikProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
