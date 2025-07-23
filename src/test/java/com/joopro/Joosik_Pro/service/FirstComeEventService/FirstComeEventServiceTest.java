cbf78043c53ec4640342bf390469b3b52bfd283d
junhyukkkk <cjunk0304@gmail.com>
2025-07-23 15:50:18 +0900

    [#29]FirstComeEventServiceTest에 빈 주입 확실히 해놓기

--- src/test/java/com/joopro/Joosik_Pro/service/FirstComeEventService/FirstComeEventServiceTest.java
+++ src/test/java/com/joopro/Joosik_Pro/service/FirstComeEventService/FirstComeEventServiceTest.java
@@ -4,6 +4,7 @@ import com.joopro.Joosik_Pro.domain.FirstComeEventParticipation;
 import com.joopro.Joosik_Pro.repository.FirstComeEventRepository.FirstComeEventRepositoryV1;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 })
 public class FirstComeEventServiceTest {
 
+    @Qualifier("v3")
     @Autowired
     private FirstComeEventService eventService;
 
